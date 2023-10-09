5015fb77b692b897e34e92313a88fb7334ad2281
JM_ARAUJO <jmaz.dev@gmail.com>
2023-10-09 12:10:43 -0300

    courses interface/ service/ layout, pipe category

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { CursosService } from '../services/cursos.service';
+import { Course } from '../interfaces/course';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.scss']
+  styleUrls: ['./courses.component.scss'],
 })
-export class CoursesComponent {
+export class CoursesComponent implements OnInit {
+  courses$: Observable<Course[]>;
+  displayedColumns = ['name', 'category'];
 
+  constructor(
+    private coursesSrv: CursosService,
+    private _snackBar: MatSnackBar
+  ) {
+    this.courses$ = this.coursesSrv.getAllCourses().pipe(
+      catchError((err) => {
+        console.error(err.message);
+        _snackBar.open('Ocorreu um erro: ' + err.status, undefined, {
+          duration: 3000,
+        });
+        return of([]);
+      })
+    );
+  }
+
+  ngOnInit(): void {
+    this.listCourses();
+  }
+  listCourses() {}
 }
---
+++ src/app/courses/services/cursos.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { first, tap, delay } from 'rxjs';
+import { Course } from '../interfaces/course';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CursosService {
+  constructor(private httpClient: HttpClient) {}
+
+  private readonly API = '/assets/courses.json';
+
+  getAllCourses() {
+    return this.httpClient.get<Course[]>(this.API).pipe(first(), delay(0));
+  }
+}
