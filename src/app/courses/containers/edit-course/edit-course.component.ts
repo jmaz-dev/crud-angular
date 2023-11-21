11df089403013b6b70d6837b45e660d6486176f0
JM_ARAUJO <jmaz.dev@gmail.com>
2023-11-21 15:25:28 -0300

    d

--- src/app/courses/containers/edit-course/edit-course.component.ts
+++ src/app/courses/containers/edit-course/edit-course.component.ts
@@ -29,7 +29,7 @@ export class EditCourseComponent {
           this.onSuccess();
           setTimeout(() => {
             window.location.reload();
-          }, 1500);
+          }, 500);
         }
       },
     });
